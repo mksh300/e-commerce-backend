e89bbb67588f60feed014782a252a9d044f4980d
mayank-sharma-appinventiv-2 <138752838+mayank-sharma-appinventiv-2@users.noreply.github.com>
2024-06-03 16:22:21 +0530

    order products routes added

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const order_product = require('../models/order_product');
+const OrderProduct = require('../models/order_product');
+const Order = require('../models/order');
 
 // create a order
 router.get('/', async (req, res) => {
   try {
-    
-    res.json([]);
+    const orderProduct = new OrderProduct({
+        userId: req.body.userId,
+        productId: req.body.productId,
+      });
+      const newOrderProduct = await orderProduct.save();
+    res.json(newOrderProduct);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 // update a order
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-      
-      res.json([]);
+       const orderProduct = await Order.findById(req.params.id);
+       orderProduct.status = req.body.status
+       const newOrderProduct = await orderProduct.save()
+       res.json(newOrderProduct);
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
   });
 
-  // delete a order
-router.get('/', async (req, res) => {
-    try {
-      
-      res.json([]);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
   
